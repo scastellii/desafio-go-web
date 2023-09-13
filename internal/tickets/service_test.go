646a7cb80840039f964f0f8f561df189e95d4cc6
scastellii <santiago.castelli@mercadolibre.com>
2023-09-13 13:33:03 -0300

    Fix test

--- internal/tickets/service_test.go
+++ internal/tickets/service_test.go
@@ -106,7 +106,7 @@ func TestGetTicketByDestination(t *testing.T) {
 	repo := NewRepositoryTest(dbMock)
 	service := NewService(repo)
 
-	tkts, err := service.GetTotalTickets(cxt, "China")
+	tkts, err := service.GetTotalTickets(nil, "China")
 
 	assert.Nil(t, err)
 	assert.True(t, dbMock.spy)
@@ -123,7 +123,7 @@ func TestGetTotalTickets(t *testing.T) {
 	repo := NewRepositoryTest(dbMock)
 	service := NewService(repo)
 
-	avr, err := service.AverageDestination(cxt, "China")
+	avr, err := service.AverageDestination(nil, "China")
 
 	assert.Nil(t, err)
 	assert.NotNil(t, avr)
